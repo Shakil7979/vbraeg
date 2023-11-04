e34b9c64a65ca7ea366e5b12694669dcf483a7eb
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2023-11-04 15:44:51 +0600

    update

---
+++ assets/js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".review_carousel").owlCarousel({
+        loop:true, 
+        nav:false,
+        dots:false,
+        items:2.4,
+        margin: 50,
+        autoplay:true
+    });
+})
