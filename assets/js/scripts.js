604f43e6d535d4833ad40a80977f791360349265
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2023-11-05 22:31:47 +0600

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -16,4 +16,42 @@ $(document).ready(function(){
         autoplay:true,
         navText: ["<img src='assets/images/blog/prev.png'>","<img src='assets/images/blog/next.png'>"]
     });
+
+
+    $(document).on('click','.banner_tab_click', function(){
+       var img_path =  $(this).attr('img_path');  
+       var style_path =' url('+ img_path +') no-repeat scroll center center / cover';
+       $('.banner_bg').css('background', style_path);
+       $('.banner_tab_click').removeClass('active');
+       $(this).addClass('active');
+       
+       var data_id =  $(this).attr('data_id');   
+
+       if(data_id == 1){
+         $('.banner_content').hide();
+         $('.banner_first_first').delay(400).show();
+       }
+       else if (data_id == 2){
+         $('.banner_content').hide();
+         $('.banner_first_second').delay(400).show();
+       }
+       else if (data_id == 3){
+         $('.banner_content').hide();
+         $('.banner_first_third').delay(400).show();
+       }
+       else if (data_id == 4){
+         $('.banner_content').hide();
+         $('.banner_first_four').delay(400).show();
+       }
+       else if (data_id == 5){
+         $('.banner_content').hide();
+         $('.banner_first_five').delay(400).show();
+       }
+       
+        return false;
+    });
+
+
+
+
 })
